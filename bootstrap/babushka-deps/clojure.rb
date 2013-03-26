02ba09fed03a2683d348f6919f27b8ead1b8456f
Andrew Kiellor <akiellor@gmail.com>
2013-03-26 03:08:10 -0700

    Fail hard on shell failures.

--- bootstrap/babushka-deps/clojure.rb
+++ bootstrap/babushka-deps/clojure.rb
@@ -9,7 +9,7 @@ dep 'leiningen' do
 
   met? { lein_bin.p.exists? }
   meet {
-    shell "curl https://raw.github.com/technomancy/leiningen/stable/bin/lein > #{lein_bin} && chmod 755 #{lein_bin}"
+    shell! "curl https://raw.github.com/technomancy/leiningen/stable/bin/lein > #{lein_bin} && chmod 755 #{lein_bin}"
   }
 end
 
@@ -21,8 +21,8 @@ dep 'cassou ppa' do
   requires 'python-software-properties.managed'
 
   met? { File.exists?("/etc/apt/sources.list.d/cassou-emacs-precise.list") }
-  meet { shell "add-apt-repository -y ppa:cassou/emacs" }
-  after { shell "apt-get update" }
+  meet { shell! "add-apt-repository -y ppa:cassou/emacs" }
+  after { shell! "apt-get update" }
 end
 
 dep 'python-software-properties.managed' do
