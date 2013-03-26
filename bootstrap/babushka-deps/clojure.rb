6d1f638cb5ebda6ed2d95c732c9d51c2cc92020d
Andrew Kiellor <akiellor@gmail.com>
2013-03-26 02:07:51 -0700

    Installs emacs24

--- bootstrap/babushka-deps/clojure.rb
+++ bootstrap/babushka-deps/clojure.rb
@@ -1,6 +1,30 @@
 dep 'clojure' do
   requires 'java.managed'
   requires 'clojure.managed'
+  requires 'emacs24'
+end
+
+dep 'emacs24' do
+  requires 'emacs24.managed'
+end
+
+dep 'emacs24.managed' do
+  requires 'cassou ppa'
+end
+
+dep 'cassou ppa' do
+  requires 'python-software-properties.managed'
+
+  met? { File.exists?("/etc/apt/sources.list.d/cassou-emacs-precise.list") }
+  meet { shell "add-apt-repository -y ppa:cassou/emacs" }
+  after { shell "apt-get update" }
+end
+
+dep 'python-software-properties.managed' do
+  installs {
+    via :apt, %w{python-software-properties}
+  }
+  provides %w{add-apt-repository}
 end
 
 dep 'clojure.managed' do
