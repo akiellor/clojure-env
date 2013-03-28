dac0183be6af58df04e3bcca1abebe382e93f256
Andrew Kiellor <akiellor@gmail.com>
2013-03-27 23:39:27 -0700

    Install nrepl and clojure-mode.

--- bootstrap/babushka-deps/clojure.rb
+++ bootstrap/babushka-deps/clojure.rb
@@ -1,6 +1,6 @@
 dep 'clojure' do
   requires 'java.managed'
-  requires 'emacs24.managed'
+  requires 'emacs24'
   requires 'leiningen'
 end
 
@@ -10,6 +10,38 @@ dep 'leiningen' do
   met? { lein_bin.p.exists? }
   meet {
     shell! "curl https://raw.github.com/technomancy/leiningen/stable/bin/lein > #{lein_bin} && chmod 755 #{lein_bin}"
+  }
+end
+
+dep 'emacs24' do
+  requires 'emacs24.managed'
+  requires 'emacs24.config'
+end
+
+dep 'emacs24.config' do
+  require 'fileutils'
+  
+  emacs_config_path = "/home/vagrant/.emacs.d/init.el"
+  emacs_config_dir_path = "/home/vagrant/.emacs.d"
+  user = 'vagrant'
+  content = <<-EOS
+(require 'package)
+(add-to-list 'package-archives
+  '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(defvar my-packages '(clojure-mode nrepl))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+  EOS
+  
+  met? { File.file?(emacs_config_path) && Etc.getpwuid(File.stat(emacs_config_dir_path).uid).name == user && File.read(emacs_config_path) == content }
+  meet {
+    FileUtils.makedirs(File.dirname(emacs_config_path))
+    FileUtils.chown_R(user, user, File.dirname(emacs_config_path))
+    File.open(emacs_config_path, "w") {|f| f << content }
   }
 end
 
