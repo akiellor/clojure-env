a03af3dc3a8ab06d85565d698ad815696281a5fd
Andrew Kiellor <akiellor@gmail.com>
2013-04-08 19:49:13 -0700

    Extracted file meta to files repo.

--- bootstrap/babushka-deps/clojure.rb
+++ bootstrap/babushka-deps/clojure.rb
@@ -19,30 +19,7 @@ dep 'emacs24' do
   requires 'emacs24.config'
 end
 
-meta "file" do
-  accepts_value_for :source
-  accepts_value_for :target
-  accepts_value_for :owner, "root"
-  accepts_value_for :group, "root"
-  accepts_value_for :permissions, "644"
-
-  template {
-    def source_path
-      dependency.load_path.parent / source
-    end
-
-    met? { target.p.file? && target.p.owner == owner && target.p.group == group && File.stat(target).mode.to_s(8)[3..5] == permissions && source_path.read == target.p.read } 
-    meet {
-      target.p.open("w+") do |f|
-        f << source_path.read
-      end
-      shell! "chown #{owner}:#{group} #{target}"
-      shell! "chmod #{permissions} #{target}"
-    }
-  }
-end
-
-dep 'emacs24.config', :template => "file" do
+dep 'emacs24.config', :template => "file-deps:file" do
   owner "vagrant"
   group "vagrant"
   
